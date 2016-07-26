e2f7817980f0975f5f470b5e2c529e14466fbc46
gaoyinghao <gaoyinghaosx@163.com>
2016-07-27 00:47:49 +0800

    类型转换演示

---
+++ src/com/thinkinginjava/chapter3/CastingNumbers.java
@@ -0,0 +1,24 @@
+package com.thinkinginjava.chapter3;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * 类型转换演示
+ * @ClassName:CastingNumbers
+ * @Description: 类型转换演示
+ * @author gaoyinghaosx@163.com
+ * @date:2016年7月27日 上午12:47:36
+ */
+public class CastingNumbers {
+
+	public static void main(String[] args) {
+		double above = 0.7, below = 0.4;
+		float fabove = 0.7f, fbelow = 0.4f;
+		print("(int)above:" + (int) above); // 0
+		print("(int)below:" + (int) below); // 0
+		print("(int)fabove:" + (int) fabove); // 0
+		print("(int)fbelow:" + (int) fbelow); // 0
+
+	}
+
+}
