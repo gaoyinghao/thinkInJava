fed3ef62b04d83425d2d3f858ebb782842a846c3
gaoyinghao <gaoyinghaosx@163.com>
2016-07-27 00:50:30 +0800

    while循环演示

---
+++ src/com/thinkinginjava/chapter4/WhileTest.java
@@ -0,0 +1,26 @@
+package com.thinkinginjava.chapter4;
+
+/**
+ * while循环演示
+ * @ClassName:WhileTest
+ * @Description:while循环演示
+ * @author gaoyinghaosx@163.com
+ * @date:2016年7月27日 上午12:49:58
+ */
+public class WhileTest {
+
+	static boolean condition() {
+		boolean result = Math.random() < 0.99;
+		System.out.println(result + ".");
+		return result;
+	}
+
+	public static void main(String[] args) {
+		while (condition()) {
+			System.out.println("Inside 'while'");
+			System.out.println("Exited 'while'");
+		}
+
+	}
+
+}
