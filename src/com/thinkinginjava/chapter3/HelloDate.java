71597cc3560f07458c41fe3faea59177817acdf2
gaoyinghao <gaoyinghaosx@163.com>
2016-07-27 00:24:53 +0800

    更简单的打印语句（静态导入）

---
+++ src/com/thinkinginjava/chapter3/HelloDate.java
@@ -0,0 +1,20 @@
+package com.thinkinginjava.chapter3;
+
+import java.util.Date;
+import static net.mindview.util.Print.*;
+
+/**
+ * 更简单的打印语句（静态导入）
+ * @ClassName:HelloDate
+ * @Description:更简单的打印语句（静态导入）
+ * @author gaoyinghaosx@163.com
+ * @date:2016年7月24日 下午11:40:59
+ */
+public class HelloDate {
+
+	public static void main(String[] args) {
+		print("Hello, it is");
+		print(new Date());
+	}
+
+}
