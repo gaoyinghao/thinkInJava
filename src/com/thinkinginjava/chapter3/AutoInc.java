be80154e8b58563dba5e2959a0379db1c9f310d8
gaoyinghao <gaoyinghaosx@163.com>
2016-07-27 00:36:38 +0800

    自动递增和递减

---
+++ src/com/thinkinginjava/chapter3/AutoInc.java
@@ -0,0 +1,35 @@
+package com.thinkinginjava.chapter3;
+
+import static net.mindview.util.Print.*;
+
+/**
+ * 自动递增和递减
+ * @ClassName:AutoInc
+ * @Description:自动递增和递减
+ * @author gaoyinghaosx@163.com
+ * @date:2016年7月27日 上午12:35:52
+ */
+public class AutoInc {
+
+	public static void main(String[] args) {
+		int i = 1;
+		print("i :" + i);
+		print("++i :" + ++i);
+		print("i++ :" + i++);
+		print("i :" + i);
+		print("--i :" + --i);
+		print("i-- :" + i--);
+		print("i :" + i);
+
+		/*
+		 * 结果：
+		 	i :1
+			++i :2
+			i++ :2
+			i :3
+			--i :2
+			i-- :2
+			i :1
+		 */
+	}
+}
