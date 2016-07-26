61d3a542c27e1ce7ba951cb27bfabba0413d9e85
gaoyinghao <gaoyinghaosx@163.com>
2016-07-27 00:26:19 +0800

    操作符的优先级

---
+++ src/com/thinkinginjava/chapter3/Precedence.java
@@ -0,0 +1,23 @@
+package com.thinkinginjava.chapter3;
+
+/**
+ * 操作符的优先级
+ * 
+ * @ClassName:Precedence
+ * @Description:操作符的优先级
+ * @author gaoyinghaosx@163.com
+ * @date:2016年7月25日 上午12:00:16
+ */
+public class Precedence {
+
+	public static void main(String[] args) {
+		int x = 1, y = 2, z = 3;
+		int a = x + y - 2 / 2 + z;
+		int b = x + (y - 2) / (2 + z);
+		System.out.println("a=" + a + ",b=" + b);
+		
+		//a=5,b=1
+
+	}
+
+}
