b0574a7d493b82b6135758aa8322d22a935d2fd0
gaoyinghao <gaoyinghaosx@163.com>
2016-07-27 00:44:28 +0800

    指数计数法演示

---
+++ src/com/thinkinginjava/chapter3/Exponents.java
@@ -0,0 +1,26 @@
+package com.thinkinginjava.chapter3;
+
+/**
+ * 指数计数法演示
+ * @ClassName:Exponents
+ * @Description:指数计数法演示
+ * @author gaoyinghaosx@163.com
+ * @date:2016年7月27日 上午12:44:06
+ */
+public class Exponents {
+	public static void main(String[] args) {
+		float expFloat = 1.39e-43f;
+		expFloat = 1.39E-43f;
+		System.out.println(expFloat);
+		double expDouble = 47e47d;
+		double expDouble2 = 47e47;
+		System.out.println(expDouble);
+		
+		
+		/*
+		  结果：
+		    1.39E-43
+			4.7E48
+		 */
+	}
+}
